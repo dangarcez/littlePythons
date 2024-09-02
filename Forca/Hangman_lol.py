340ab521a0a876725b55be3ff2d4e583cd36c528
Daniel Garcez <179493995+dangarcez@users.noreply.github.com>
2024-09-02 14:11:38 -0300

    Jogo Handman finalizado

--- Forca/Hangman_lol.py
+++ Forca/Hangman_lol.py
@@ -1,14 +1,28 @@
-palavra = "Abrobinha"
-palavraDesvendada = palavra
-letraCoringa = "*"
-pLista = list(letraCoringa*len(palavra))
-vidas = len(palavra)+2
+import random
+with open("personagens.txt",'r') as file:
+   texto = file.read()
+   table = texto.maketrans("\n ",",,","1234567890.");
+   texto = texto.translate(table)
+   palavras = texto.split(',')
 
-print(f"Hangman Game!\nA palavra é:{''.join(pLista)}\n")
+palavra = random.choice(palavras)
+palavraDesvendada = palavra
+letraCoringa = "_"
+pLista = list(letraCoringa*len(palavra))
+vidas = len(palavra)
+
+print(f"Hangman Game! Adivinhe qual é o campeão de League of Legends\nA palavra é:{''.join(pLista)}\n")
 while True:
    num = -1
    letra = input("Informe uma letra: ").lower()
    flag = False
+
+   if(len(letra)>1):
+      print("Apenas uma letra permitida!\n")
+      continue
+   elif len(letra)<1:
+      print("Você precisa informar uma letra\n")
+      continue
 
    while((num := palavra.lower().find(letra,num+1))!=-1):
       flag = True
